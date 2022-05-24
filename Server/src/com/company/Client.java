2bb4aad0f8bf8eeb45ddf14e003e87e28088b2b9
leochiessa <leonardochiessa@gmail.com>
2022-05-23 21:21:46 -0300

    client output terminal fix

--- Server/src/com/company/Client.java
+++ Server/src/com/company/Client.java
@@ -6,30 +6,30 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client extends Thread {
-    private final Socket clientStocket;
+    private final Socket clientSocket;
 
-    public Client(Socket clientStocket) {
-        this.clientStocket = clientStocket;
+    public Client(Socket clientSocket) {
+        this.clientSocket = clientSocket;
     }
 
     @Override
     public void run() {
         try {
-            PrintWriter out = new PrintWriter(clientStocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientStocket.getInputStream()));
-            System.out.println("Client " + clientStocket.getInetAddress() + " connected.");
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("Client " + clientSocket.getInetAddress() + " connected.");
             String line = "";
             do {
                 if (!line.equals("")) {
-                    System.out.println(clientStocket.getInetAddress() + " says: " + line);
+                    System.out.println(clientSocket.getInetAddress() + " says: " + line);
                     out.println("\n" + line);
                 }
             } while ((!(line = in.readLine()).equalsIgnoreCase("x")));
             out.println("\nBye.");
             in.close();
             out.close();
-            clientStocket.close();
-            System.out.println("Client " + clientStocket.getInetAddress() + " disconnected.");
+            clientSocket.close();
+            System.out.println("Client " + clientSocket.getInetAddress() + " disconnected.");
         } catch (Exception e) {
         }
     }
